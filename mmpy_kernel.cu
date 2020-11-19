8a8762b7faa4b8980646886eb858542861ed5886
hjl-yul154 <hjl_liangyuxiao@126.com>
2020-11-19 04:44:08 -0800

    Update mmpy_kernel.cu

--- mmpy_kernel.cu
+++ mmpy_kernel.cu
@@ -9,14 +9,52 @@ using namespace std;
 
 #include <stdio.h>
 
+#define BLOCKTILE_M 96
+#define BLOCKTILE_N 64
+#define BLOCKTILE_K 32
+#define THREADTILE_M (BLOCKTILE_M/BLOCKDIM_Y)
+#define THREADTILE_N (BLOCKTILE_N/BLOCKDIM_X)
+
+#define BY BLOCKDIM_Y
+#define BX BLOCKDIM_X
+#define BK BLOCKTILE_K
+#define BM BLOCKTILE_M
+#define BN BLOCKTILE_N
+#define TM THREADTILE_M
+#define TN THREADTILE_N
 #define get_mat(mat,N,i,j)((i<N)&&(j<N)?mat[i*N+j]:0)
+
+//
+#define BLOCK_SIZE_M 96
+#define BLOCK_SIZE_N 64
+#define BLOCK_SIZE_K 32
+#if BLOCK_SIZE_M % BLOCKDIM_Y || BLOCK_SIZE_K % BLOCKDIM_X
+#error Use thread block to load block of A
+#endif
+#if BLOCK_SIZE_K % BLOCKDIM_Y || BLOCK_SIZE_N % BLOCKDIM_X
+#error Use thread block to load block of B
+#endif
+#if BLOCK_SIZE_M % BLOCKDIM_Y || BLOCK_SIZE_N % BLOCKDIM_X
+#error Use thread block to compute block of C
+#endif
+// Number of sub-block of C for each thread
+#define X_SUB (BLOCK_SIZE_N / BLOCKDIM_X)
+#define Y_SUB (BLOCK_SIZE_M / BLOCKDIM_Y)
+
+#define MAT(mat, N, i, j) (mat[(i)*N + (j)])
+#define MAT_PADDED(mat, N, i, j) ((i) < N && (j) < N ? MAT(mat, N, i, j) : 0)
+#define A_ELEMENT(i, j) MAT_PADDED(A, N, i, j)
+#define B_ELEMENT(i, j) MAT_PADDED(B, N, i, j)
+#define C_ELEMENT(i, j) MAT(C, N, i, j)
+//
+
 
 __global__ void matMul(int N, _DOUBLE_ *C, _DOUBLE_ *A, _DOUBLE_ *B) {
     __shared__ _DOUBLE_ Ab[BM][BK];
     __shared__ _DOUBLE_ Bb[BK][BN];
     _DOUBLE_ frag_a[TM];
     _DOUBLE_ frag_b[TN];
-    _DOUBLE_ Cb[TM][TN]={0};
+    _DOUBLE_ c[TM][TN]={0};
 
 
     int ty = threadIdx.y, tx=threadIdx.x;
@@ -63,16 +101,16 @@ __global__ void matMul(int N, _DOUBLE_ *C, _DOUBLE_ *A, _DOUBLE_ *B) {
         }
         __syncthreads();
     }
-    #pragma unroll
-    for(int i=0;i<TM;++i){
-        #pragma unroll
-        for(int j=0;j<TN;++j){
-            if(I+i*BY<N && J+j*BX<N){
-                C[(I+BY*i)*N+J+BX*j]=Cb[i][j];
+#pragma unroll
+    for (int i = 0; i < Y_SUB; ++i) {
+#pragma unroll
+        for (int j = 0; j < X_SUB; ++j) {
+            if (I0 + ty + i * BLOCKDIM_Y < N && J0 + tx + j * BLOCKDIM_X < N) {
+                C_ELEMENT(I0 + ty + i * BLOCKDIM_Y, J0 + tx + j * BLOCKDIM_X) =
+                    c[i][j];
             }
         }
     }
-
 }
 
 
